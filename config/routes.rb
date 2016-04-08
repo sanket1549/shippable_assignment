0411db7e6217fe2e465f7f197c78eac632c9b621
sanket1549 <sanket@gmail.com>
2016-04-08 19:42:09 +0530

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'home' => 'static_pages#home'
 
-  get 'help' => 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
